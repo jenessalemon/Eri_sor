639aecbd00ce3c74383b424ba39015265ce44ddd
Your Name <jimblotter@Jenessa.local>
2017-04-20 16:46:48 -0600

    here i go

--- find_barcodes.R
+++ find_barcodes.R
@@ -1,20 +1,22 @@
 setwd("/Users/jimblotter/Desktop/Grad_School/Data_Analysis/barcodes_pre_analysis")
 barcodes_file <- read.csv("barcodes.csv")  # read csv file 
 barcodes <- data.frame(barcodes_file)
+
 barcodes[1,1]
-nrow(barcodes) #565
-ncol(barcodes) #2
-barcodes["Sample"]     #or barcodes$Sample, displays columns
-barcodes["003-15",]    #returns NAs
-barcodes[["GH8", exact = TRUE]]   #returns NULL
+barcodes["Sample"]                            #or barcodes$Sample, displays columns
+barcodes[15,]                                 #returns the 15th row
+
+barcodes$Sample == "003-15" || "028-01"                  #this works, returning TRUE or FALSE, but not in a loop for some reason
+barcodes[grep("003", barcodes$Samples), ]
+grep("003-15", barcodes$Samples)
 
 for (i in barcodes$Sample) {
   if (i == "003-15") {
-    return (barcodes[003-15,])
+    return (barcodes["003-15",])
   }
 }
 
-psuedocode:
+here is my psuedocode:
 if the value in the (sample) column starts with 003,
 return the corresponding calue in the (barcode) column
 
