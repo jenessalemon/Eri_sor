75f2b11acc311a05ff85609011d40b301366a5c4
Jenessa Lemon <u6009817@ember1.privatearch.arches>
2017-06-23 20:34:44 -0600

    step 2 for filter and rm cd in erisor

--- filter_SLURM.sh
+++ filter_SLURM.sh
@@ -1,14 +1,15 @@
 #!/bin/csh
 #SBATCH --time=72:00:00          # walltime, abbreviated by -t
 #SBATCH --nodes=1                # number of cluster nodes, abbreviated by -N
-#SBATCH -o ipyrad-%j.out-%N	 # name of the stdout, using the job number (%j) and the first n$
-#SBATCH -e ipyrad-%j.err-%N	 # name of the stderr, using job and first node values
+#SBATCH -o filter-%j.out-%N         # name of the stdout, using the job number (%j) and the first n$
+#SBATCH -e filter-%j.err-%N         # name of the stderr, using job and first node values
 #SBATCH --ntasks=1               # number of MPI tasks, abbreviated by -n
 # additional information for allocated clusters
 #SBATCH --account=wolf           # account - abbreviated by -A
 #SBATCH --partition=ember        # partition, abbreviated by -p
 #SBATCH --mail-type=END
 #SBATCH --mail-user=jenessa.lemon@gmail.com
+
 #
 # set data and working directories
 setenv WORKDIR $HOME/new_eri
@@ -18,8 +19,9 @@ cp -r $WORKDIR/* $SCRDIR
 cd $SCRDIR
 #
 # run the program
-/uufs/chpc.utah.edu/common/home/u6009817/miniconda2/bin/ipyrad -p params-erisor.txt -s 7
+/uufs/chpc.utah.edu/common/home/u6009817/miniconda2/bin/ipyrad -p params-filter.txt -s 2
 #
 # transfer output and remove scratch
-rsync -vz erisor.vcf /uufs/chpc.utah.edu/common/home/u6009817/new_eri/
+cd ..
+rsync -zv *.fastq.gz /uufs/chpc.utah.edu/common/home/u6009817/new_eri
 #rm -r $SCRDIR
